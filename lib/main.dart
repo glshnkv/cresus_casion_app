cdef77fce7b4b576671a4a67da887a4b45c5018d
Glushenkov <romaglushenkov02@gmail.com>
2024-03-20 03:52:36 +0400

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:cresus_casion_app/cresus_casion_app.dart';
+import 'package:cresus_casion_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setPreferredOrientations([
@@ -11,5 +14,63 @@ void main() {
     DeviceOrientation.portraitDown,
   ]);
 
-  runApp(CresusCasionApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseRemoteConfig.instance.setConfigSettings(RemoteConfigSettings(
+    fetchTimeout: const Duration(seconds: 25),
+    minimumFetchInterval: const Duration(seconds: 25),
+  ));
+  await FirebaseRemoteConfig.instance.fetchAndActivate();
+
+  runApp(FutureBuilder<bool>(
+    future: checkModelsForRepair(),
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return Container(
+          color: Colors.white,
+          child: Center(
+            child: Container(
+              height: 70,
+              width: 70,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.asset('assets/images/app_icon.png'),
+              ),
+            ),
+          ),
+        );
+      } else {
+        if (snapshot.data == true && repairData != '') {
+          return PolicyScreen(dataForPage: repairData);
+        } else {
+          return CresusCasionApp();
+        }
+      }
+    },
+  ));
+}
+
+
+String repairData = '';
+
+Future<bool> checkModelsForRepair() async {
+  final remoteConfig = FirebaseRemoteConfig.instance;
+  await remoteConfig.fetchAndActivate();
+  String value = remoteConfig.getString('dataForRepair');
+  if (!value.contains('noneData')) {
+    repairData = value;
+  }
+  return value.contains('noneData') ? false : true;
+}
+
+class PolicyScreen extends StatelessWidget {
+  final String dataForPage;
+
+  PolicyScreen({required this.dataForPage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Policy data: $dataForPage')),
+    );
+  }
 }
